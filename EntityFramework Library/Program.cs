d5518274dda5cc5c533b7cdb97527c92c706fad7
iArttt <volk19011@gmail.com>
2026-06-17 10:34:26 +0300

    Add project files.

---
+++ EntityFramework Library/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFramework_Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
